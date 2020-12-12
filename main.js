a2055015271d44ea6653fbc19048c3a8aa3db104
Anwesha Priyam <72214354+Anwesha-Priyam@users.noreply.github.com>
2020-12-12 16:51:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,57 +13,61 @@ window.addEventListener("keydown",my_keydown);
 function my_keydown(e)
 {
     keycode=e.keycode;
-    if(e.shiftkey==true&&keydown==80)
+    if(e.shiftkey==true&&keydown=='80')
     {
         block_height=block_height-10;
         block_width=block_width-10;
+        document.getElementById("width").innerHTML=block_width;
+        document.getElementById("height").innerHTML=block_height;
     }
-    if(e.shiftkey==true&&keydown==77)
+    if(e.shiftkey==true&&keydown=='77')
     {
         block_height=block_height+10;
         block_width=block_width+10;
+        document.getElementById("width").innerHTML=block_width;
+        document.getElementById("height").innerHTML=block_height;
     }
-    if(keycode==70)
+    if(keycode=='70')
     {
         new_Image("spiderman_face.png");
         console.log("f");
     }
-    if(keycode==66)
+    if(keycode=='66')
     {
         new_Image("ironman_body.png");
         console.log("b");
     }
-    if(keycode==76)
+    if(keycode=='76')
     {
         new_Image("hulk_legs.png");
         console.log("l");
     }
-    if(keycode==82)
+    if(keycode=='82')
     {
         new_Image("thor_right_hand.png");
         console.log("r");
     }
-    if(keycode==72)
+    if(keycode=='72')
     {
         new_Image("captain_america_left_hand.png");
         console.log("r");
     }
-    if(keycode==38)
+    if(keycode=='38')
     {
         up();
         console.log("up");
     }
-    if(keycode==40)
+    if(keycode=='40')
     {
         down();
         console.log("down");
     }
-    if(keycode==37)
+    if(keycode=='37')
     {
         left();
         console.log("left");
     }
-    if(keycode==39)
+    if(keycode== '39')
     {
         right();
         console.log("right");
